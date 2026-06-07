5d2c1e66083bf9375cb8e457d469be82b46d8e3c
jyx-07 <s25069@gsm.hs.kr>
2026-06-07 23:45:36 +0900

    init

---
+++ src/main/kotlin/team/nongchun/hororog/HororogApplication.kt
@@ -0,0 +1,11 @@
+package team.nongchun.hororog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HororogApplication
+
+fun main(args: Array<String>) {
+	runApplication<HororogApplication>(*args)
+}
---
+++ src/test/kotlin/team/nongchun/hororog/HororogApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.nongchun.hororog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HororogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
