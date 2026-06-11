c1a919c11737fc91c3c5de6e5ff53cf74c018938
jyx-07 <s25069@gsm.hs.kr>
2026-06-12 00:33:00 +0900

    refactor(auth): 로그아웃의 불필요한 JPA 트랜잭션 제거

--- src/main/kotlin/team/nongchun/hororog/domain/member/service/LogoutServiceImpl.kt
+++ src/main/kotlin/team/nongchun/hororog/domain/member/service/LogoutServiceImpl.kt
@@ -1,12 +1,10 @@
 package team.nongchun.hororog.domain.member.service
 
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import team.nongchun.hororog.global.auth.AuthenticationHolder
 import team.nongchun.hororog.global.auth.RefreshTokenRepository
 
 @Service
-@Transactional(readOnly = true)
 class LogoutServiceImpl(
     private val authenticationHolder: AuthenticationHolder,
     private val refreshTokenRepository: RefreshTokenRepository,
