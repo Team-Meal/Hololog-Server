40ec07645f5cf817ffb5d9e17d370fed8c17feab
jyx-07 <s25069@gsm.hs.kr>
2026-06-12 22:32:25 +0900

    fix(admin): N+1 방지를 위해 findByStatus에 EntityGraph 적용

--- src/main/kotlin/team/nongchun/hororog/domain/member/repository/NutritionistSignupRequestRepository.kt
+++ src/main/kotlin/team/nongchun/hororog/domain/member/repository/NutritionistSignupRequestRepository.kt
@@ -2,6 +2,7 @@ package team.nongchun.hororog.domain.member.repository
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import team.nongchun.hororog.domain.member.entity.NutritionistSignupRequest
 import team.nongchun.hororog.domain.member.entity.SignupStatus
@@ -12,6 +13,7 @@ interface NutritionistSignupRequestRepository : JpaRepository<NutritionistSignup
         status: SignupStatus,
     ): Boolean
 
+    @EntityGraph(attributePaths = ["member"])
     fun findByStatus(
         status: SignupStatus,
         pageable: Pageable,
