769a2ff1d13b7079ea8a0b28c03920a18fbfd650
jyx-07 <s25069@gsm.hs.kr>
2026-06-30 17:32:19 +0900

    fix(member): CreateSignupRequestRequest 네이밍 컨벤션 위반 수정

--- src/main/kotlin/team/nongchun/hororog/domain/member/service/CreateSignupRequestService.kt
+++ src/main/kotlin/team/nongchun/hororog/domain/member/service/CreateSignupRequestService.kt
@@ -1,8 +1,8 @@
 package team.nongchun.hororog.domain.member.service
 
-import team.nongchun.hororog.domain.member.dto.CreateSignupRequestRequest
+import team.nongchun.hororog.domain.member.dto.CreateSignupRequest
 import team.nongchun.hororog.domain.member.dto.SignupRequestResponse
 
 interface CreateSignupRequestService {
-    fun execute(request: CreateSignupRequestRequest): SignupRequestResponse
+    fun execute(request: CreateSignupRequest): SignupRequestResponse
 }
